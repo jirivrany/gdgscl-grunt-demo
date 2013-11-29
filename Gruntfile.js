6013d47f5466fdfcdcdfa0b42b01b3847b903cc8
Jiri Vrany <jiri.vrany@tul.cz>
2013-11-29 17:52:59 +0100

    finalni verze / refaktoring a odstraneni chyb nalezenych lintem

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,65 +7,70 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
     grunt.initConfig({
-        // grunt-closure-compiler https://github.com/gmarty/grunt-closure-compiler
-        'closure-compiler': {
-            frontend: {
-                closurePath: 'tools/closure',
-                js: 'app/calc.js',
-                jsOutputFile: 'dist/calc.js',
-                maxBuffer: 500,
-                options: {
-                    compilation_level: 'ADVANCED_OPTIMIZATIONS',
-                    language_in: 'ECMASCRIPT5_STRICT'
+            // grunt-closure-compiler https://github.com/gmarty/grunt-closure-compiler
+            'closure-compiler': {
+                frontend: {
+                    closurePath: 'tools/closure',
+                    js: 'app/calc.js',
+                    jsOutputFile: 'dist/calc.js',
+                    maxBuffer: 500,
+                    options: {
+                        compilation_level: 'ADVANCED_OPTIMIZATIONS',
+                        language_in: 'ECMASCRIPT5_STRICT'
+                    }
                 }
-            }
-        },
-        // grunt-contrib-cssmin vice na https://github.com/gruntjs/grunt-contrib-cssmin
-        cssmin: {
+            },
+            // grunt-contrib-cssmin vice na https://github.com/gruntjs/grunt-contrib-cssmin
+            cssmin: {
 
-            minify: {
-                files: {
-                    'dist/style.css': 'app/style.css'
+                minify: {
+                    files: {
+                        'dist/style.css': 'app/style.css'
+                    }
                 }
-            }
-        },
-        // Task htmlmin https://github.com/gruntjs/grunt-contrib-htmlmin
-        htmlmin: {
-            dist: { // Target
-                options: { // Target options
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: { // Dictionary of files
-                    'dist/index.html': 'app/index.html' // 'destination': 'source'
+            },
+            // Task htmlmin https://github.com/gruntjs/grunt-contrib-htmlmin
+            htmlmin: {
+                dist: { // Target
+                    options: { // Target options
+                        removeComments: true,
+                        collapseWhitespace: true
+                    },
+                    files: { // Dictionary of files
+                        'dist/index.html': 'app/index.html' // 'destination': 'source'
+                    }
                 }
-            }
-        },
-        watch: { // Watch https://github.com/gruntjs/grunt-contrib-watch
-            scripts: {
-                files: ['Gruntfile.js', 'app/*.js'],
-                tasks: ['build'],
-                options: {
-                    spawn: false,
+            },
+            watch: { // Watch https://github.com/gruntjs/grunt-contrib-watch
+                scripts: {
+                    files: ['Gruntfile.js', 'app/*.js'],
+                    tasks: ['build'],
+                    options: {
+                        spawn: false,
+                    },
                 },
             },
-        },
-        // grunt-contrib-jshint pro statickou kontrolu https://github.com/gruntjs/grunt-contrib-jshint
-        jshint: {
-            all: ['Gruntfile.js', 'app/*.js']
-        }
-    });
+            // grunt-contrib-jshint pro statickou kontrolu https://github.com/gruntjs/grunt-contrib-jshint
+            jshint: {
+                ignore_warning: {
+                    options: {
+                        '-W061': true,
+                    },
+                    all: ['Gruntfile.js', 'app/*.js']
+                }
+            }    
+            });
 
-    // Build task.
-    grunt.registerTask('build', [
-        'jshint',
-        'closure-compiler',
-        'cssmin',
-        'htmlmin'
-    ]);
+        // Build task.
+        grunt.registerTask('build', [
+            'jshint',
+            'closure-compiler',
+            'cssmin',
+            'htmlmin'
+        ]);
 
-    // Default task.
-    grunt.registerTask('default', ['watch']);
+        // Default task.
+        grunt.registerTask('default', ['watch']);
 
 
-};
+    };
--- app/calc.js
+++ app/calc.js
@@ -1,156 +1,149 @@
-var Calc = (function() {
-    var MIN_RESULT_FONT_SIZE = 16;
-    var MAX_CHARS_SHOWABLE_IN_RESULT_INPUT = 1000;
+(function () {
+    'use strict';
+    var calc = calc || {},
+        MIN_RESULT_FONT_SIZE = 16,
+        MAX_CHARS_SHOWABLE_IN_RESULT_INPUT = 1000,
+        resultInput = null,
+        basicTable = null,
+        credits = null,
+        container = null,
+        viewportWidth = null,
+        viewportHeight = null,
+        calcStr = '';
 
-    var resultInput = null;
-    var basicTable = null;
-    var credits = null;
-    var container = null;
-    var viewportWidth = null;
-    var viewportHeight = null;
-    var keyboard = null;
-
-    var calcStr = "";
 
     /**
-     * Calc class constructor
-     * @constructor
+     * APP init
      */
-    return new function() {
-        var _self = this;
+    calc.init = function () {
+        resultInput = document.getElementById('resultInput');
 
-        /**
-         * APP init
-         */
-        this.init = function() {
-            resultInput = document.getElementById("resultInput");
+        basicTable = document.getElementById('basic');
+        credits = document.getElementById('credits');
+        container = document.getElementById('container');
 
-            basicTable = document.getElementById("basic");
-            credits = document.getElementById("credits");
-            container = document.getElementById("container");
+        document.addEventListener('touchstart', calc.getButtonHandler, false);
 
-            document.addEventListener('touchstart', function(event) {
+        calc.redraw();
+    };
 
-                switch (event.target.id) {
-                    case 'backspace':
-                        _self.backspace();
-                        break;
-                    case 'equals':
-                        _self.equals();
-                        break;
-                    case 'clear':
-                        _self.clear();
-                        break;
-                    case 'memset':
-                        _self.memSet();
-                        break;
-                    case 'memret':
-                        _self.memRet();
-                        break;
-                    default:
-                        _self.append(event.target.attributes['data-char'].value);
-                };
-            }, false);
+    calc.getButtonHandler = function (event) {
 
-            _self.redraw();
+        var handlers = {
+            'backspace': calc.backspace,
+            'equals': calc.equals,
+            'clear': calc.clear,
+            'memset': calc.memSet,
+            'memret': calc.memRet
+        };
+
+        if (handlers[event.target.id]) {
+            handlers[event.target.id]();
+        } else {
+            calc.append(event.target.attributes['data-char'].value);
         }
 
-        /**
-         * Redraw display po zmene velikosti
-         */
-        this.redraw = function() {
-            // viewport: http://javascript.about.com/od/browserobjectmodel/a/bom10.htm, http://www.quirksmode.org/dom/w3c_cssom.html#windowview
-            viewportWidth = parseInt(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth, 10);
-            viewportHeight = parseInt((window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight) * 0.98, 10);
-            container.style.width = viewportWidth + "px";
-            container.style.height = viewportHeight + "px";
-            // font size
-            var fontSize = parseInt(resultInput.offsetHeight * 0.9, 10);
-            if (MIN_RESULT_FONT_SIZE > fontSize) fontSize = MIN_RESULT_FONT_SIZE;
-            document.body.style.fontSize = fontSize + "px";
-            MAX_CHARS_SHOWABLE_IN_RESULT_INPUT = parseInt(1.7 * resultInput.offsetWidth / fontSize, 10) - 1;
-            // table width 
-            basicTable.style.width = (viewportWidth < basicTable.offsetHeight ? parseInt(viewportWidth * 0.99, 10) : basicTable.offsetHeight) + "px";
-        };
 
-        /**
-         * Update display / vola se po provedeni operace
-         */
-        this.update_result_display = function() {
-            if (calcStr.length >= MAX_CHARS_SHOWABLE_IN_RESULT_INPUT)
-                resultInput.value = calcStr.substr(calcStr.length - MAX_CHARS_SHOWABLE_IN_RESULT_INPUT);
-            else
-                resultInput.value = calcStr;
-        };
-
-        /**
-         * Add value to display
-         * @param {string} value - single char po keypress
-         */
-        this.append = function(value) {
-            calcStr += value;
-            _self.update_result_display();
-        };
-
-        /**
-         * CLS / clear key handler
-         */
-        this.clear = function() {
-            calcStr = "";
-            _self.update_result_display();
-        };
-
-        /**
-         * Set display to local storage
-         */
-        this.memSet = function() {
-            window.localStorage.calcmem = calcStr;
-            _self.update_result_display();
-        };
-
-        /**
-         * Get value from local storage / set it to display
-         */
-        this.memRet = function() {
-            calcStr = window.localStorage.calcmem !== undefined ? window.localStorage.calcmem : calcStr;
-            _self.update_result_display();
-        };
-
-        /**
-         * Backspace key handler
-         */
-        this.backspace = function() {
-            if (1 <= calcStr.length)
-                calcStr = calcStr.substr(0, calcStr.length - 1);
-            _self.update_result_display();
-        };
+    };
 
 
-        /**
-         * Eval display value
-         * otestovat displej a zobrazit vypoctenou hodnotu
-         */
-        this.equals = function() {
-            var str = resultInput.value;
-            str = str.replace(/x/g, "*");
-            if (_self.validateMathExp(resultInput.value)) {
-                calcStr = eval(str);
-                _self.update_result_display();
-            }
+    /**
+     * Redraw display po zmene velikosti
+     * by Ramesh Nair (www.hiddentao.com)
+     */
+    calc.redraw = function () {
+        // viewport: http://javascript.about.com/od/browserobjectmodel/a/bom10.htm, http://www.quirksmode.org/dom/w3c_cssom.html#windowview
+        viewportWidth = parseInt(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth, 10);
+        viewportHeight = parseInt((window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight) * 0.98, 10);
+        container.style.width = viewportWidth + 'px';
+        container.style.height = viewportHeight + 'px';
+        // font size
+        var fontSize = parseInt(resultInput.offsetHeight * 0.9, 10);
+        if (MIN_RESULT_FONT_SIZE > fontSize) fontSize = MIN_RESULT_FONT_SIZE;
+        document.body.style.fontSize = fontSize + 'px';
+        MAX_CHARS_SHOWABLE_IN_RESULT_INPUT = parseInt(1.7 * resultInput.offsetWidth / fontSize, 10) - 1;
+        // table width 
+        basicTable.style.width = (viewportWidth < basicTable.offsetHeight ? parseInt(viewportWidth * 0.99, 10) : basicTable.offsetHeight) + 'px';
+    };
+
+    /**
+     * Update display / vola se po provedeni operace
+     */
+    calc.updateResultDisplay = function () {
+        if (calcStr.length >= MAX_CHARS_SHOWABLE_IN_RESULT_INPUT)
+            resultInput.value = calcStr.substr(calcStr.length - MAX_CHARS_SHOWABLE_IN_RESULT_INPUT);
+        else
+            resultInput.value = calcStr;
+    };
+
+    /**
+     * Add value to display
+     * @param {string} value - single char po keypress
+     */
+    calc.append = function (value) {
+        calcStr += value;
+        calc.updateResultDisplay();
+    };
+
+    /**
+     * CLS / clear key handler
+     */
+    calc.clear = function () {
+        calcStr = '';
+        calc.updateResultDisplay();
+    };
+
+    /**
+     * Set display to local storage
+     */
+    calc.memSet = function () {
+        window.localStorage.calcmem = calcStr;
+        calc.updateResultDisplay();
+    };
+
+    /**
+     * Get value from local storage / set it to display
+     */
+    calc.memRet = function () {
+        calcStr = window.localStorage.calcmem !== undefined ? window.localStorage.calcmem : calcStr;
+        calc.updateResultDisplay();
+    };
+
+    /**
+     * Backspace key handler
+     */
+    calc.backspace = function () {
+        if (1 <= calcStr.length)
+            calcStr = calcStr.substr(0, calcStr.length - 1);
+        calc.updateResultDisplay();
+    };
+
+
+    /**
+     * Eval display value
+     * otestovat displej a zobrazit vypoctenou hodnotu
+     */
+    calc.equals = function () {
+        var str = resultInput.value;
+        str = str.replace(/x/g, '*');
+        if (calc.validateMathExp(resultInput.value)) {
+            calcStr = eval(str);
+            calc.updateResultDisplay();
         }
+    };
 
-        /**
-         * Pred vypoctem pomoci eval kontrola povolenych znaku
-         * @param {string} formula - vzorec smi obsahovat pouze znaky 0-9+-/x.
-         */
-        this.validateMathExp = function(formula) {
-            var mathRegExp = /^[0-9x\.\+-\/]*$/;
-            return mathRegExp.test(formula);
-        };
+    /**
+     * Pred vypoctem pomoci eval kontrola povolenych znaku
+     * takze se eval nemusime bat az tak moc...
+     * @param {string} formula - vzorec smi obsahovat pouze znaky 0-9+-/x.
+     */
+    calc.validateMathExp = function (formula) {
+        var mathRegExp = /^[0-9x\.\+-\/]*$/;
+        return mathRegExp.test(formula);
+    };
 
 
-    }
-})();
 
-window.addEventListener('load', Calc.init, false);
-window.addEventListener('resize', Calc.redraw, false);
+    window.addEventListener('load', calc.init, false);
+    window.addEventListener('resize', calc.redraw, false);
+}());
